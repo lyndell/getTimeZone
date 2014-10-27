df3d5231892d700e29927634c1ed388d6a2158a8
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2014-10-26 21:48:02 -0500

    shuffle, tidy code, more output from API

--- getTZ.php
+++ getTZ.php
@@ -2,21 +2,16 @@
 
     include('api.php');
 
-    // default to Lihue, Hawaii timezone
-    date_default_timezone_set ('Pacific/Honolulu');
-    $latitude  =   21.53;
-    $longitude = -159.22;
-
     // Quyery Houston timezone
     $latitude  =   29.75;
     $longitude =  -95.25;
-    $request_url = "http://api.timezonedb.com/?lat=" . $latitude . "&lng=" . $longitude ."&key=" . $api_key . "&format=json";
+    $request_url = "http://api.timezonedb.com/?format=json" .
+      "&key=" . $api_key .
+      "&lat=" .  $latitude .
+      "&lng=" .  $longitude ;
     $content = file_get_contents($request_url);
 
     echo "URL: " . $request_url;
-    echo "\n\n";
-    echo "Raw JSON dump: \n";
-    var_dump($content);
     echo "\n\n";
     $content = json_decode($content);
     echo "Decoded JSON object: \n";
@@ -28,11 +23,16 @@
     // status OK.
     //
     echo "Yay JSON successfully decoded.\n";
-    echo "API status is: " . $content->status . "\n";
+    echo "API status is: " . $content->status    . "\n";
+    echo "Timezone is:   " . $content->zoneName  . "\n";
+    echo "Time stamp:    " . $content->timestamp . "\n";
   }
   else
   {
-    // default to Hawaii
+    // default to Lihue, Hawaii timezone
+    date_default_timezone_set ('Pacific/Honolulu');
+    $latitude  =   21.53;
+    $longitude = -159.22;
     echo "Lihue, HI: \n";
     echo "Timezone is: Pacific/Honolulu \n";
     echo "\n\n";
